6906660ee241c8615501f6e32df81df6b10ad8dc
LeonardoContino <leoconti2000@hotmail.it>
2023-01-12 16:35:47 +0100

    js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("js ok");
+// console.log("js ok");
+
+const app = Vue.createApp({
+
+    data() {
+        return {
+            user: {
+                name: 'Nome Utente',
+                avatar: '_io'
+              },
+              contacts: [
+                {
+                  name: 'Michele',
+                  avatar: '_1',
+                },
+                {
+                  name: 'Fabio',
+                  avatar: '_2',
+                },
+                {
+                  name: 'Samuele',
+                  avatar: '_3',
+                },
+                {
+                  name: 'Luisa',
+                  avatar: '_4',
+                },
+              ]
+    
+        };
+
+}})
+app.mount('#root')
